c21a5d31454b185a6d8c6bf012e5def2ee118167
Cidgarfield <cidgarfield@yahoo.com.br>
2025-09-01 22:04:47 -0300

    :sparkles: feat: implementacao de conexao com banco e rota post

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print('Olá Mundo!!')
+from fastapi import FastAPI, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from typing import List, Optional
+
+import models,schemas
+from database import SessionLocal,engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title='API Catálogo de Filmes')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/")
+def home():
+    return{'mensagem':'Bem-Vindo ao API de Filmes :)'}
+
+@app.post('/filmes', response_model=schemas.Filme)
+def criar_filme(filme:schemas.FilmeCreate,db:Session=Depends(get_db)):
+    novo_filme = models.Filme(**filme.dict())
+    db.add(novo_filme)
+    db.commit()
+    db.refresh(novo_filme)
+    return novo_filme
---
+++ schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class FilmeBase(BaseModel):
+
+    titulo:str
+    genero:str
+    ano:int
+    nota:float
+
+class FilmeCreate(FilmeBase):
+    pass
+
+class Filme(FilmeBase):
+    id_filme:int
+
+    class Config:
+        orm_mode = True
